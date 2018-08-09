f3f857b183401b1ec300c0226ea0b8075a2dfa7a
Max Weston <max@connected-ventures.com>
2018-08-09 17:13:30 +0100

    more

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var Xray = require("x-ray");
 var x = Xray();
+var sportinglife;
+var wiki;
 
-x("https://www.sportinglife.com/", {
-  document: x(["p", "h1", "h2", "h3", "h4", "section", "header"]),
-  ad: x(".fresh8 .container", ["*"])
-}).write("page-results.json");
+x("https://www.sportinglife.com/", [
+  "p",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "section",
+  "header"
+])(function(err, data) {
+  // console.log(data);
+  sportinglife = data;
+});
 
+var wiki = x("https://en.wikipedia.org/wiki/Gambling", [
+  "p",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "section",
+  "header"
+])(function(err, data) {
+  // console.log(data);
+  wiki = data;
+});
+
+console.log(sportinglife);
+console.log(wiki);
 // x("https://en.wikipedia.org/wiki/Gambling", [
 //   fresh8: ".fresh8"
 // ]).write("other-results.json");
